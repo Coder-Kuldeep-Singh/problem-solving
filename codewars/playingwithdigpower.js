61ed312cd44a3205d330d79c670cd17d5a950a9a
mavensingh <kuldeep.avsar@gmail.com>
2020-08-06 13:39:58 +0530

    program to play with powers

---
+++ codewars/playingwithdigpower.js
@@ -0,0 +1,11 @@
+function digPow(n, p) {
+  return p;
+  //   var Itoa = n.toString();
+  //   for (var i = 0; i < Itoa.length; i++) {
+  //     var Atoi = Itoa[i].toString();
+  //     console.log(Atoi);
+  //   }
+}
+
+var val = digPow(46288, 51);
+console.log(val);
